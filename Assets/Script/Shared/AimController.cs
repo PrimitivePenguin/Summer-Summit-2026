27366f10a2b8df9a0aadb58b216eb0be6a6cd3bc
PrimitivePenguin <browngerbil2006@gmail.com>
2026-09-02 20:23:02 -0700

    AimController updated
    actively turning

--- Assets/Script/Shared/AimController.cs
+++ Assets/Script/Shared/AimController.cs
@@ -6,20 +6,55 @@ using UnityEngine;
 // Call AimAt() at target
 public class AimController : MonoBehaviour
 {
+    [Header("Turn")]
+    public bool isTurnRate = true;
+    public float turnRate; // degrees per second
+    private float currentAngle;
+    public float GetFacingAngle() => currentAngle;
+
+    // called by EnemyController.cs with target position, requires EnemyData -> implement later
+    // public void Initialize(float turnRate)
+    // {
+    //     this.turnRate = isTurnRate ? turnRate : Mathf.Infinity; // if turnRate is 0, set to infinity
+    // }
+    // Awake() replaces Initialize
+    void Awake()
+    {
+        currentAngle = transform.eulerAngles.z;
+        turnRate = isTurnRate ? turnRate : Mathf.Infinity; // if turnRate is 0, set to infinity
+    }
+
+
+    // Raw angle calc
     public float GetAngleTo(Vector2 targetPos)
     {
-
         Vector2 direction = targetPos - (Vector2)transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg -90f; // bullet.cs uses translate up -> 90 deg mismatch
-        // Debug.DrawRay(transform.position, direction.normalized * 3f, Color.red);
         return angle;
     }
+
     // Set rotation cone from targetPos in center of the cone, with angle from bulletSpawnData
     public void AimAt(Vector2 targetPos, BulletSpawn bulletSpawn)
     {
-        float angle = GetAngleTo(targetPos);
-        // Debug.Log($"[AimController] Target: {targetPos}, Current Position: {(Vector2)transform.position}, angle: {angle}");
+        float targetAngle = GetAngleTo(targetPos);
+        // change current angle by time * tunrate based on current + target angle
+        // rotate object to face target position
+        currentAngle = Mathf.MoveTowardsAngle(currentAngle, targetAngle, turnRate * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(0, 0, currentAngle); 
+
+        // Debug.Log($"[AimController] Target: {targetPos}, Current Position: {(Vector2)transform.position}, angle: {targetAngle}");
         float halfAngle = bulletSpawn.GetCurrentData().spreadAngle / 2f;
-        bulletSpawn.SetFiringArc(angle - halfAngle, angle + halfAngle);
+        bulletSpawn.SetFiringArc(currentAngle - halfAngle, currentAngle + halfAngle);
+    }
+    // aim to snap
+    public void SnapTo(float angle)
+    {
+        currentAngle = angle;
+        transform.rotation = Quaternion.Euler(0, 0, currentAngle); 
+    }
+    public bool IsAimedAt(Vector2 targetPos, float tolerance = 5f)
+    {
+        float targetAngle = GetAngleTo(targetPos);
+        return Mathf.Abs(Mathf.DeltaAngle(currentAngle, targetAngle)) < tolerance;
     }
 }
